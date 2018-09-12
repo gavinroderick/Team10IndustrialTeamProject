3c1cff3472459905188e09102abf9241ebfe44e4
Warren Mansell <wmansell@dundee.ac.uk>
2018-09-12 14:35:05 +0100

    Catching up with github

--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ module.exports = function(app) {
     app.route('/')
         .get((req,res) => res.render('pages/index'));
     app.route('/TestMap')
-        .get((req,res) => res.render('pages/TestMap')); 
+        .get((req,res) => res.render('pages/TestMap'));
+    app.route('/Maps')
+    .get((req,res) => res.render('pages/Map'));
 }
