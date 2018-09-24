d36c102cbf74ca2c7f2adb8fb812df60b66da457
Marcus Robertson-Jones <marcusrobertsonjones@gmail.com>
2018-09-24 14:33:10 +0100

    made data generation work for both floors

--- public/js/Generato.js
+++ public/js/Generato.js
@@ -1,27 +1,18 @@
+    // Including Moment.js stuff
     const moment = require('moment');
     moment().format();
-    var IDs = [];  
-    //let IDs: any[] = [];
-     
-    var Total = 2;
-    //debug("meme");
-    //console.log("meme")
-    // var i;
-    // var j;
-    // var current = Math.random();
-    // var newNo = SmoothRandom(0.1,current);
-    // for (i = 0; i < Total; i++) 
-    // { 
-    //     //console.log("0900");
-    //     for (j = 0; j < 10; j++)
-    //     {
-    //         //console.log(newNo);
-    //         current = newNo;
-    //         newNo = SmoothRandom(0.2,current);
-    //     }
-    // }
-    getID();
-   // generateData();
+
+    // Global var to store IDs of shops
+    //var groundIDs = [];  
+
+    //this function starts the process
+    getID('http://team10-itp.herokuapp.com/api/groundFloorStores.json','groundData');
+    getID('http://team10-itp.herokuapp.com/api/firstFloorStores.json','firstData');
+
+    /*
+        Generates Random numbers within a certain range of the previous number
+        Adapted from part of this: http://bl.ocks.org/telic/9376360
+    */
     function SmoothRandom(factor,start) 
     {
         var max = Math.min(1, start + factor);
@@ -29,86 +20,73 @@
         return start = Math.random() * (max - min) + min;
     }
 
-    function getID()
+    /*
+        Reads the Store IDs from a file
+        then calls the the generateStructures() function as that can't start without the data from getID()
+    */
+    function getID(idLocation,outFileName)
     {
+        var IDs = [];
         var temp;
         const request = require('request');
-       // var IDs = [];
-       request('https://api.myjson.com/bins/15bmcg',{json: true}, function (error, response, body) 
+        request(idLocation,{json: true}, function (error, response, body) 
         {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            //console.log(body);
 
             temp = body;
             
             for(var i = 0; i < temp.stores.length; i++){
                 IDs.push(temp.stores[i].id);
-                //console.log(temp.stores[i].id);
             }
             console.log(IDs.length); 
-            createStructures();
-            //return IDs;  
-        });
-      // return IDs
-      
+            createStructures(outFileName,IDs); 
+        }); 
     }
+
+    /*
+        Generates 21 dates back in time from current date
+        returns them as an array of moment objects
+    */
     function generateDates()
     {
-        //console.log("test");
         var dates = [];
         var today = new moment().utc().set({'hour': 9,'minute': 0,'second': 0,'millisecond': 0});
         for (var i = 0; i<21; i++)
         {
-            //dates[i]=[]; 
-            //var today = moment();
             dates.push(today.add(-1, 'day').toDate());
-            // for (var j = 1; j<4; j++)
-            // {
-            //     console.log(today);    
-            //     today = moment.utc(dates[i][j-1]);   
-            //     console.log(today);
-            //     dates[i][j] = today.add(15, 'minute').toDate();
-            //    // console.log(dates[i][j]);
-            //    console.log(dates[i][j]);
-            // }
-            // today = moment().utc().set({'day': -i ,'hour': 9, 'minute': 0,'seconds': 0});
-    
-            
-            //console.log(dates[i]);
-            
+            /*  
+            /   Might be useful later
+            /   dates[i][j] = today.add(15, 'minute').toDate();
+            /   today = moment().utc().set({'day': -i ,'hour': 9, 'minute': 0,'seconds': 0});
+            */ 
         }
-        
         return dates;
     }
+
+    /* 
+        Generates a list of 40 timeslots, 1 every 15 minutes from 0900 - 1900
+        returns them as an array or moment objects 
+    */
     function generateTimes(date)
     {
         var times = [];
-        //console.log(date);
         var current = moment(date);
         times.push(date);
       
         for (var i = 1; i<40; i++)
         {
-           // times.push("hi");
-           // console.log("hi");
             times.push(current.add(15, 'minute').toDate());
         }
-       // console.log(times);
+
        return times;
 
     }
-    function generateData()
+    /*
+        Combines all the created data into an apropriate json structure
+    */
+    function createStructures(fileName,IDs)
     {
-        IDs = getID(createStructures);
-        //while(IDs ){
-        //    console.log('hi');
-        //} 
-        
-    }
-    function createStructures()
-    {
-       // console.log(IDs.length);
         var shops = [IDs.length];
         console.log("hi")
         console.log(IDs.length);
@@ -118,7 +96,6 @@
                 'id': IDs[i],
                 'history': null
             }
-           // console.log("testing");
             var dates = generateDates();
             var days = [21];
             for (var k = 0 ; k < 21; k++)
@@ -127,9 +104,9 @@
                     'date': dates[k],               
                     'times': null
                 }
-                //console.log(history);
                 var times = generateTimes(dates[k]);
-                //console.log(times[0])
+
+                // Generate initial values
                 var currentOccupancy = Math.random();
                 var currentNoise = Math.random();
                 var currentHumidity = Math.random();
@@ -154,29 +131,26 @@
                 history['times'] = slots;
                 days[k] = history;
             }
-           // console.log(dates[i]);
-            
-            //var days = {
-
-            //}
             shop['history'] = days;
             shops[i] = shop;  
             
         }
-        console.log(shops);
-        writeToFile(shops);
+        writeToFile(shops,fileName);
+       
 
-        //var newDateObj = new Date(oldDateObj.getTime() + diff*60000)
-       //console.log(dates);
     }
-    function writeToFile(object){
+
+    /*
+        Writes an object to a file keeping nice formatting
+        Adapted from: https://gyandeeps.com/json-file-write/
+    */
+    function writeToFile(object,fileName){
         var fs = require("fs");
-        fs.writeFile("./DATA.json", JSON.stringify(object, null, 4), (err) => {
+        fs.writeFile("../api/" + fileName +".json", JSON.stringify(object, null, 4), (err) => {
             if (err) {
                 console.error(err);
                 return;
             };
-            console.log("File has been created");
         });
     }
 
