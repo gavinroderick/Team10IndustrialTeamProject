15540df2e8bc8696649f70a266253872f2774762
Warren Mansell <wmansell@dundee.ac.uk>
2018-09-25 15:39:15 +0100

    Demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var del = require('del');
+var vinyl = require('vinyl-paths');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('js', function(){
@@ -7,10 +9,16 @@ gulp.task('js', function(){
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/js/'))
+    .pipe(gulp.dest('public/js/min/'))
 });
 
-gulp.task('default', gulp.series('js', function(done) { 
+gulp.task('delete', function(){
+	return gulp.src('public/js/min/*')
+		.pipe(vinyl(del))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', gulp.series('delete','js', function(done) { 
   console.log("Glug glug, gulp is running");
   done();
 }));
--- public/js/popups.js
+++ public/js/popups.js
@@ -7,6 +7,9 @@ var map = L.Wrld.map("map", "9d876646f7d83cc709edbe204c81d546", {
   var currentIndoorMapId;
   var currentFloor;
   var entityIdsToPosition = {};
+  var d;
+  var popup;
+  
   
   var lastMouseDown;
   function onMouseDown(event) {
@@ -22,6 +25,8 @@ var map = L.Wrld.map("map", "9d876646f7d83cc709edbe204c81d546", {
   }
   
   function onIndoorMapEntered(event) {
+    map.indoors.setFloor(0);
+    map.setView([56.4598, -2.9728], 17);
     currentIndoorMapId = event.indoorMap.getIndoorMapId();
     currentFloor = map.indoors.getFloor().getFloorIndex();
   }
@@ -29,27 +34,142 @@ var map = L.Wrld.map("map", "9d876646f7d83cc709edbe204c81d546", {
   function onIndoorMapFloorChanged() {
     currentFloor = map.indoors.getFloor().getFloorIndex();
   }
+
+  function getStoreName(id){
+    if(currentFloor == 0)
+    {
+      for(var i = 0; i < groundFloor.stores.length; i++)
+      {
+        if(groundFloor.stores[i].id == id)
+        {
+          //console.log(groundFloor.stores[i].store);
+          return groundFloor.stores[i].store;
+        }
+      }
+    }
+  }
+
+  function validId(id) {
+
+    for( var i = 0; i < groundFloor.stores.length; i++)
+    {
+          if (storeData[i]['id'] == id)
+          {
+              return i;
+          }
+    }
+    for( var i = 0; i < firstFloor.stores.length; i++)
+    {
+          if (storeData[i]['id'] == id)
+          {
+              return i;
+          }
+    }
+    return null;
+}
+
+
+//For Demo
+var counter = 1;
+var currentHour = 9;
+var currentMin = 0;
   
   function identifyEntity(id) {
     var latLng = lastMouseDown;
+    d = new Date();
 
-    var graph = drawBasic(id);
-    
-    var popupOptions = { 
-      indoorMapId: currentIndoorMapId, 
-      indoorMapFloorIndex: currentFloor, 
-      autoClose: false, 
-      closeOnClick: false,
-      minWidth: "5"          
-    };
-    var popup = L.popup(popupOptions)
-      .setLatLng(latLng)
-      .addTo(map)
-      .setContent(graph);
-    entityIdsToPosition[id] = { "latLng": latLng, "indoorId": currentIndoorMapId, "floorIndex": currentFloor } ;
+    var valid = validId(id);
+
+    if(valid != null)
+    {
+      //map.setView(latLng, 17);
+
+      d = displayTime();
+      
+      var popupOptions = { 
+        indoorMapId: currentIndoorMapId, 
+        indoorMapFloorIndex: currentFloor, 
+        autoClose: true, 
+        closeOnClick: true,
+        minWidth: "700"          
+      };
+      popup = L.popup(popupOptions)
+        .setLatLng(latLng)
+        .addTo(map)
+        .setContent(createMockHTMLElement(id, d, fakeDrawBasic(id)));
+      entityIdsToPosition[id] = { "latLng": latLng, "indoorId": currentIndoorMapId, "floorIndex": currentFloor } ;
+
+      //For demo
+      if (counter == 0)
+      {
+        currentHour = currentHour + 1;
+        currentMin = 0;
+        console.log(currentHour);
+        console.log(currentMin);
+      } else {
+        currentMin = currentMin + 15;
+        console.log(currentHour);
+        console.log(currentMin);
+      }
+      if(counter == 3)
+      {
+        counter = 0;
+      }else{
+        counter++;
+      }
+    }
+
+    // setTimeout(updatePopup(id, d), 5000);
+  }
+
+  function updatePopup(id, d)
+  {
+    d = new Date();
+    console.log(d.toLocaleTimeString('it-IT'));
+    popup.setContent(createMockHTMLElement(id, d, drawBasic(id)));
   }
 
   map.indoors.on("indoormapenter", onIndoorMapEntered);
   map.indoors.on("indoormapfloorchange", onIndoorMapFloorChanged)
   map.indoors.on("indoorentityclick", onIndoorEntityClicked);
   map.on("mousedown", onMouseDown);
+
+  function createMockHTMLElement(id, date, graphText){
+    var graphHTML = '<div class="content">' +
+                    '<h2>'+ getStoreName(id) +  ' ' + date + '</h2>' +
+                    graphText +
+                    '</div>';
+    return graphHTML;
+}
+
+  function displayTime(){
+    var toReturn = "0";
+    var temp = "";
+    if (currentHour < 10 && currentMin == 0)
+    {
+        toReturn = toReturn.concat(currentHour).concat(":").concat("0").concat(currentMin);
+    } else if (currentHour < 10 && currentMin != 0)
+    {
+        toReturn = toReturn.concat(currentHour).concat(":").concat(currentMin);
+    } else if ( currentHour >= 10 && currentMin == 0)
+    {
+      toReturn = temp.concat(currentHour).concat(":").concat("0").concat(currentMin);
+    } else 
+    {
+      toReturn = temp.concat(currentHour).concat(":").concat(currentMin);
+    }
+    return toReturn;
+  }
+
+
+
+
+
+/*                  '<div class="content">' +
+                    '<h2>'+ getStoreName(id) + ' for the ' + d.getDate() + ' of Spetember 2018</h2>' +
+                    graphText +
+                    '</div>';
+*/
+
+
+
