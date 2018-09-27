3093eb6f921a2e1b6fd646e91c7d400bd28a7a48
AndreiLins <andreilins96@gmail.com>
2018-09-27 12:16:43 +0100

    changed sliders logic

--- public/js/sliders.js
+++ public/js/sliders.js
@@ -1,20 +1,30 @@
 // Called when a slider is updated. 
 function updateMapFromSliders(){ 
-
     var stores = storeData;
     var current = getCurrentSliderLevel();
-    var level = Math.round(getAverageSliderLevel(current));
-    var max = getMaxToleranceLevel(level);
-    var min = getMinToleranceLevel(level);
+    var sliderOccupancy = current[0]/5;
+    var sliderNoise = current[1]/5;
+    var sliderHumidity = current[2]/5;
+    console.log("Slider Values: " + sliderOccupancy + " " + sliderNoise + " " + sliderHumidity);
 
     for(var i = 0; i < stores.length; i++){
         var storeID = stores[i].id;
-        //var currentData = getCurrentData(storeID, Math.round(level));
-        var dataArray = getCurrentTimeslot(storeID);
-        console.log(dataArray, i);
-        var colour = calculateColour(dataArray, max, min); 
+        var currentOccupancy = stores[i].history[0].times[0].occupancy;
+        var currentNoise = stores[i].history[0].times[0].noise;
+        var currentHumidity = stores[i].history[0].times[0].humidity;
 
-        setStoreHighlights(storeID, colour);
+        console.log("Values: " + currentOccupancy + " " + currentNoise + " " + currentHumidity);
+
+        
+        if(currentOccupancy <= sliderOccupancy && currentNoise <= sliderNoise && currentHumidity <= sliderHumidity){
+            setStoreHighlights(storeID, [108,249,32, 200]); //green
+        }
+        else if(currentOccupancy > sliderOccupancy && currentNoise > sliderNoise && currentHumidity > sliderHumidity){
+        	setStoreHighlights(storeID, [249,71,32, 200]); //red 
+        }
+        else{
+        	setStoreHighlights(storeID, [249,239,32, 200]); //yellow
+        }
     }
 
 }
@@ -29,107 +39,4 @@ function getCurrentSliderLevel(){
     });
     console.log("Slider Values: " + sliderArray);
     return sliderArray;
-}
-
-
-// Takes an array of numbers (int)
-// Returns the average of the array.
-// Code for avaraging arrays found via Google at https://gist.github.com/bmorelli25/4564b8ff35b47d8a9db6b0dda1143465
-function getAverageSliderLevel(sliderArray){
-    const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
-    return arrAvg(sliderArray);
-};
-
-function getMaxToleranceLevel(sliderAvg){
-    var maxTolerable;
-    switch(sliderAvg){
-        case 1:
-        case 2:
-            return maxTolerable = 0.4;
-            break;
-        case 3:
-            return maxTolerable = 0.6;
-            break;
-        case 4:
-        case 5:
-            return maxTolerable = 0.9;
-            break;
-    }
-}
-function getMinToleranceLevel(sliderAvg){
-    var minTolerable;
-
-    switch(sliderAvg){
-        case 1:
-        case 2:
-            return minTolerable = 0.1;
-            break;
-        case 3:
-            return minTolerable = 0.3;
-            break;
-        case 4:
-        case 5:
-            return minTolerable = 0.6;
-            break;
-    }
-}
-// parseDataObj(obj);
-//     obj
-//     currentDataArray = [];
-//     currentArray.push(currentTimeslot['occupancy']);
-//     currentArray.push(currentTimeslot['noise']);
-//     currentArray.push(currentTimeslot['humidity']);
-
-//     return calculateColour(currentArray, maxTolerable, minTolerable);
-// }
-
-function getCurrentTimeslot(id){
-    var store;
-    for( var i = 0; i < groundFloor.stores.length; i++) {
-        if (storeData[i]['id'] == id) {
-            store = i;
-            break;
-        }
-    }
-    return storeData[store]['history'][0]['times'][0];
-}
-
-function checkValue(value, max, min){
-    var colourLevel = 0;
-    if(parseFloat(value) > max){ colourLevel = 5; };
-    if(parseFloat(value) < min){ colourLevel = 1; };
-    if(parseFloat(value) < max && parseFloat(value > min)){ colourLevel = 3; };
-    console.log("Value Being Checked: " + parseFloat(value) + " Colour Level" + colourLevel);
-    return parseInt(colourLevel);
-}
-
-function calculateColour(currentDataObj, max, min){
-    var colourLevel = 0;
-    var colour;
-    var occupancy = currentDataObj.occupancy;
-    var noise = currentDataObj.noise;
-    var humidity = currentDataObj.humidity;
-    console.log("Occupany~Noise~Humidity: " + occupancy + " " + noise + " " + humidity)
-    colourLevel += checkValue(occupancy,max,min);
-    colourLevel += checkValue(noise, max, min);
-    colourLevel += checkValue(humidity, max, min);
-    
-    var avgColourLevel = Math.round(colourLevel / 3);
-    console.log(avgColourLevel);
-    switch(avgColourLevel){
-        case 1:
-        case 2:
-            colour = [255,0,0,200];
-            break;
-        case 3:
-            colour = [255,140,0,200];
-            break;
-        case 4:
-        case 5:
-            colour = [0,255,0,200];
-            break;
-        default: 
-            colour= [255,255,200, 200];
-    }
-    return colour;
 }
