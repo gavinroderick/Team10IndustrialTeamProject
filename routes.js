583d9a2cc68bd143c79f1f2995dacc84d033a7a0
Gavin Roderick <gavin.roderick26@gmail.com>
2018-09-27 22:49:02 +0100

    pretty pretty

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ module.exports = function(app){
     /*app.route('/example')
         .get((req,res) => res.render('pages/example')); <--- NOTE THE SEMICOLON */
     app.route('/')
-        .get((req,res) => res.render('pages/MapUsingJson'));
+        .get((req,res) => res.render('pages/index'));
     app.route('/map')
     	.get((req,res) => res.render('pages/MapUsingJson'));
 
