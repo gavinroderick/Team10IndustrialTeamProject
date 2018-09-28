df5be7c7bfb71911022ca5ddd2404099abcf5f2c
Gavin Roderick <gavin.roderick26@gmail.com>
2018-09-28 11:26:36 +0100

    gulping

--- public/js/settings.js
+++ public/js/settings.js
@@ -13,11 +13,11 @@ window.onload = function (){
 function presetsUpdate(level){
     var occupancy = [1,3,5];
     var noise = [1,3,5];
-    var humidity = [1,3,5];
+    var light = [1,3,5];
 
     for(var i=0; i < 3; i++){
         document.getElementById("occupancy-slider").value = occupancy[level];
         document.getElementById("noise-slider").value = noise[level];
-        document.getElementById("light-slider").value = humidity[level];
+        document.getElementById("light-slider").value = light[level];
     }
 }
